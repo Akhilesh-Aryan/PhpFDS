909ecd4c717c3d3f86be5ce5727712c39d609b44
Akhilesh-Aryan <akhilesharyanindia.com>
2021-01-01 18:09:40 +0530

    first commit

---
+++ cart.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>FDS</title>
+        <link href="bootstrap.css" rel="stylesheet">
+        <link href="style.css" rel="stylesheet">
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+            <a class="navbar-brand" href="">Online Foodie</a>
+        </nav>
+        <div class="py-3 bg-dark text-white">
+            <div class="container">
+                <h6>Home/</h6>
+                <h2>My Cart</h2>
+            </div>
+        </div>
+        <div class="container">
+           <div class="row mt-4">
+               <div class="col-lg-12">
+                   <div class="card mb-3">
+                       <div class="row">
+                           <div class="col-lg-2">
+                              <span class="veg bg-success">Veg</span>
+                               <img src="bgimage.jpg"alt="" class="w-100" height="auto">
+                           </div>
+                           <div class="col ml-5">
+                              <br>
+                              <br>
+                               <div class="card-body">
+                                   <h5>Puneer Butter Massala</h5>
+                                   <a href="" class="btn btn-danger">-</a>
+                                   <span>3</span>
+                                    <a href="" class="btn btn-danger">+</a>
+                               </div>
+                           </div>
+                           <div class="col">
+                               <br>
+                               <br>
+                               <a href="text-muted small">Remove</a>
+                           </div>
+                           <div class="col">
+                               <br>
+                               <h3>420/-</h3>
+                               <h6 class="small text-muted">140/- per Unit</h6>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+           </div> 
+        </div>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>FDS</title>
+    <link href="bootstrap.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+        <a class="navbar-brand" href="">Online Foodie</a>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item"><a href="" class="nav-link">Home</a></li>
+            <li class="nav-item"><a href="" class="nav-link">My Cart</a></li>
+        </ul>
+    </nav>
+    <div class="jumbotron" style="background-image:url('bgimage.jpg');height:500px;background-size:cover">
+        <div class="row">
+            <div class="col-lg-4 form-inline mx-auto">
+                <form action="" class="mt-2 pt-5">
+                    <h2 class="text-white">Find Your Taste</h2>
+                    <div class="input-group">
+                        <input type="search" class="form-control" size="30" placeholder="eg: burgur, Pizza etc.">
+                        <div class="input-group-append">
+                            <input type="submit" class="btn btn-dark" name="" value="Go">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="card">
+                    <span class="veg">veg</span>
+                    <img src="bgimage.jpg" alt="" class="card-image" height="200px">
+                    <div class="card-body">
+                        <h3 class="small font-weight-bolder">Paneer Veg Massala</h3>
+                        <div class="row no-gutters">
+                            <div class="col-lg-3">
+                                <h2 class="h6 mt-2">140/-</h2>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card">
+                    <span class="veg">veg</span>
+                    <img src="bgimage.jpg" alt="" class="card-image" height="200px">
+                    <div class="card-body">
+                        <h3 class="small font-weight-bolder">Paneer Veg Massala</h3>
+                        <div class="row no-gutters">
+                            <div class="col-lg-3">
+                                <h2 class="h6 mt-2">140/-</h2>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
+
+
+<?php
+
+?>
